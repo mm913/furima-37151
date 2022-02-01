17f8b7e43dc696547c4dbc5752a304d51904b18f
mm913 <mas_m_913@yahoo.co.jp>
2022-02-01 23:52:23 +0900

    registrations、sessionsのviewファイル編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname, presence: true
+  validates :birthday, presence: true
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :encrypted_password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
@@ -14,5 +15,5 @@ class User < ApplicationRecord
     validates :family_name_kana
     validates :first_name_kana
   end
-  validates :birthday, presence: true
+ 
 end

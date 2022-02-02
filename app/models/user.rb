7b4cae62a59a37dec4b879ff81a0a94c8e678acc
mm913 <mas_m_913@yahoo.co.jp>
2022-02-02 20:44:54 +0900

    新規登録、ログイン機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   validates :nickname, presence: true
   validates :birthday, presence: true
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :encrypted_password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
     validates :family_name
     validates :first_name
@@ -15,5 +15,5 @@ class User < ApplicationRecord
     validates :family_name_kana
     validates :first_name_kana
   end
- 
+  has_many :items
 end

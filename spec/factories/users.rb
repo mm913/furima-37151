2068070eb180aff166cf18ef84f173711f589880
mm913 <mas_m_913@yahoo.co.jp>
2022-02-03 21:25:56 +0900

    Rspec導入とFactoryBot記述

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname {Faker::Name.initials[number: 2]}
+    email {Faker::Internet.free_email}
+    password {Faker::Internet.password(mini_length: 6)}
+    password_confirmation {password}
+    family_name {"テスト"}
+    first_name {"太郎"}
+    family_name_kana {"テスト"}
+    first_name_kana {"タロウ"}
+    birthday  {Faker::Date.birthday(min_age: 5, max_age: 92)}
+  end
+end

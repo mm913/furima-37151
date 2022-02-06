dbc13edcb7bc5333e5fbdb2581867bc68bbcb53e
mm913 <mas_m_913@yahoo.co.jp>
2022-02-06 14:51:23 +0900

    ActiveHashのアソシエーション設定

--- app/models/delivery_days.rb
+++ app/models/delivery_days.rb
@@ -5,4 +5,8 @@ class Delivery_days < ActiveHash: :Base
     { id: 3, name: '2〜3日で発送' },
     { id: 4, name: '4〜7日で発送' }
   ]
+
+    include AcriveHash::Associations
+    has_many :items
+  
   end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,9 @@
 class Item < ApplicationRecord
   belongs_to :user
+  extend AcriveHash::Associations::AcriveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_charge
+  belongs_to :address
+  belongs_to :delivery_days
 end

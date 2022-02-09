d9c544452a056ef1eb06783996a52d79ac824d96
mm913 <mas_m_913@yahoo.co.jp>
2022-02-09 22:20:47 +0900

    販売手数料と販売利益の実装

---
+++ app/javascript/price.js
@@ -0,0 +1,16 @@
+window.addEventListener('load', () => {
+  const priceInput = document.getElementById("item-price")
+  const commission = document.getElementById("add-tax-price")
+  const gain = document.getElementById("profit")
+
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+  
+  commission.innerHTML = Math.floor(inputValue * 0.1);
+  
+  gain.innerHTML = Math.floor(inputValue - (inputValue * 0.1));
+  })
+})
+
+ 
+

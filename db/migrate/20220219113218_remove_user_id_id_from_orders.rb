4553fdb657efc81795f6022899456e3a1651a652
mm913 <mas_m_913@yahoo.co.jp>
2022-02-19 20:43:10 +0900

    カラム削除

--- db/migrate/20220219113218_remove_user_id_id_from_orders.rb
+++ db/migrate/20220219113218_remove_user_id_id_from_orders.rb
@@ -1,5 +1,5 @@
 class RemoveUserIdIdFromOrders < ActiveRecord::Migration[6.0]
   def change
-    remove_colum :orders, :user_id_id, :references
+    remove_column :orders, :user_id_id, :references
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_19_025934) do
+ActiveRecord::Schema.define(version: 2022_02_19_113218) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -64,11 +64,9 @@ ActiveRecord::Schema.define(version: 2022_02_19_025934) do
     t.bigint "item_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "user_id_id", null: false
     t.bigint "user_id", null: false
     t.index ["item_id"], name: "index_orders_on_item_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
-    t.index ["user_id_id"], name: "index_orders_on_user_id_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
